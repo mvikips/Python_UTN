19d64d52e7740e79160d0a7d09833f6b8aa63ef8
Viki Perera <pererasviky@gmail.com>
2026-06-21 12:50:38 -0300

    .

---
+++ Materiales/Ejemplos/HolaMundo.py
@@ -0,0 +1,12 @@
+#Algoritmo
+
+#Localizar impresora
+#Verificar si está encendida
+
+"""Conectar al dispositivo que contiene el archivo a ser impreso
+En el dispositivo, abrir el archivo a ser impreso
+Solicitar impresión"""
+
+
+print(
+"Hola Mundo")
